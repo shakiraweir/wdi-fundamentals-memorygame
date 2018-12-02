6c3b213651fa5ed50da4feca3bcdc8894f38969e
Shakira Weir <shakiraweir@Shakiras-MacBook-Pro.local>
2018-12-02 01:47:18 -0500

    Completed memory game

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -24,23 +24,34 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function () {
-	 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+ 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
-		alert("Sorry, try again.");
-	 }; 
+      alert("Sorry, try again.");
+	} 
 };
 
-var flipCard = function (cardID) {
-	console.log("User flipped " + cards[cardID].rank)
-	cardsInPlay.push(cards[cardID].rank);
- 	checkForMatch();
- 	console.log(cards[cardID].cardImage);
-	console.log(cards[cardID].suit);
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');	
+	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
+	}
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	} 
+};
 
+createBoard();
 
 
